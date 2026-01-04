c3c2bd428f3e60aba4a2e1475bd504c22e56d51e
Md Jobarul Islam <jobarulislam1203@gmail.com>
2026-01-05 01:36:38 +0600

    Introduce DOM manipulation examples in 09_dom.js
    Add DOM manipulation examples and comments.

---
+++ js/09_dom.js
@@ -0,0 +1,15 @@
+/*document.body.innerHTML = 'hello';
+document.title = ' Java larning in document object module';
+console.log('hello!');*/
+/*property of DOM -title*/
+/*console.log(document.title);
+document.title = 'changed!';
+console.log(document.title);*/
+/*property of DOM - body*/
+/*console.log(document.body);
+console.log(typeof document.body);
+console.log(document.body.innerHTML);
+document.body.innerHTML = '<button>good/bad</button>';
+console.log(document.body.innerHTML);*/
+console.log(document.querySelector('.a').innerHTML = 'N');
+console.log(document.querySelector('.b').innerHTML = 'button C');
