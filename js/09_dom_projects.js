2c0d064b3382f0f2755cff0ddd5a7c5901269e9f
Md Jobarul Islam <jobarulislam1203@gmail.com>
2026-01-05 13:14:04 +0600

    Update 09_dom_projects.js

--- js/09_dom_projects.js
+++ js/09_dom_projects.js
@@ -10,3 +10,17 @@ function subscribe(){
         buttonElement.innerText = 'Subscribe';
     }
 }
+
+function calculate(){
+    const inputElement = document.querySelector('.input');
+    const cost = Number(inputElement.value);
+    if (cost > 40)
+    {
+        document.querySelector('.result').innerText = `$${cost}`;
+
+    }
+    else
+        {
+            document.querySelector('.result').innerText = `$${cost + 10}` ;
+        }
+}
